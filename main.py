f32fbd847df1c8fbe7e324bd18b026f3dfbbf78d
Phan Nguyen <nguyen.phan241099@gmail.com>
2022-10-23 22:53:12 +0700

    use hough for bottle cap detection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import numpy as np
+import math
 
 def click_event(event, x, y, flags, params):
  
@@ -31,7 +32,7 @@ def setup_cam(cam_id):
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
     #cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M','J','P','G'))
     cap.set(cv2.CAP_PROP_FPS, 30)
-    # cap.set(cv2.CAP_PROP_BRIGHTNESS, 200) 
+    #cap.set(cv2.CAP_PROP_BRIGHTNESS, 240) 
     # cap.set(cv2.CAP_PROP_CONTRAST, 255)
     cap.set(cv2.CAP_PROP_SATURATION,0)
     cap.set(cv2.CAP_PROP_AUTO_WB,0)
@@ -41,74 +42,73 @@ def setup_cam(cam_id):
     #Notice when setting up webcam: CAP_V4L2, MJPG, CAP_PROP_FPS, CAP_PROP_EXPOSURE for optimal FPS in this order.
     return cap
 def bottle_cap_inspection(img):
- 
+
     img_gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     
-    _,mask= cv2.threshold(img_gray, 70,255, cv2.THRESH_BINARY)
-    mask=~mask
+    _,mask= cv2.threshold(img_gray, 15,255, cv2.THRESH_BINARY_INV)
 
-    
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
-    # mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    #cv2.imshow('mask',mask)
-        
-    img[np.where(mask==0)] = 255
-
-    # edge= cv2.Canny(mask,50,100)
-    # # cv2.imshow("edge",edge)
-    # rho = 1  # distance resolution in pixels of the Hough grid
-    # theta = np.pi / 180  # angular resolution in radians of the Hough grid
-    # threshold = 20  # minimum number of votes (intersections in Hough grid cell)
-    # min_line_length = 30  # minimum number of pixels making up a line
-    # max_line_gap = 20  # maximum gap in pixels between connectable line segments
-    # line_image = np.copy(img) * 0  # creating a blank to draw lines on
-    # lines = cv2.HoughLinesP(edge, rho, theta, threshold, np.array([]),min_line_length, max_line_gap)
-    # try:
-    #     for line in lines:
-    #         print(line)
-    #         print("a")
-    #         for x1,y1,x2,y2 in line:
-    #             cv2.line(img,(x1,y1),(x2,y2),(255,0,0),5)
-    # except:
-    #     pass
-    
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)    
     contours=sorted(contours, key=cv2.contourArea, reverse=True)
-    try:
-        if cv2.contourArea(contours[0])<15000:
-            print("No cap")
+    if len(contours)>0:
+        # print( cv2.contourArea(contours[0])) #34000
+        # print( cv2.contourArea(contours[1])) #5800
+        if cv2.contourArea(contours[0])<10000:
+            cv2.putText(img, "No cap", (520,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2,cv2.LINE_AA)
+            return img
         else:
             
-                # if len(contours)>1:
-                #     for i in range(2):
-                #         rect = cv2.minAreaRect(contours[i])
-                #         print(rect[2])
-                #         box = cv2.boxPoints(rect)
-                #         box = np.int0(box)
-                #         cv2.drawContours(img,[box],0,(127,255,0),2)
-                #         #print(i,':',cv2.contourArea(contours[i]))
-                # else:
-                #print(cv2.contourArea(contours[0]))
-                rect = cv2.minAreaRect(contours[0])
-                
-                box = cv2.boxPoints(rect)
-                box = np.int0(box)
-                print(cv2.contourArea(contours[0]))
-                print(np.sum(mask!=0))
-                print((box[0][1]+box[2][1])/2)
-                cv2.drawContours(img,[box],0,(127,255,0),2)
-                if rect[2]>-95 and rect[2]<-5:
-                    print("Crooked cap")
-                elif ((box[0][1]+box[2][1])/2)<195 or ((box[0][1]+box[2][1])/2)> 220:
-                    print("Crooked cap")
+
+            edge= cv2.Canny(mask,30,100)
+            kernel = np.ones((3, 3), np.uint8)
+            edge= cv2.dilate(edge,kernel, iterations=1)
+            cv2.imshow("edge",edge)
+            
+            rho = 1  # distance resolution in pixels of the Hough grid
+            theta = np.pi / 180  # angular resolution in radians of the Hough grid
+            threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+            min_line_length = 20  # minimum number of pixels making up a line
+            max_line_gap =  10 # maximum gap in pixels between connectable line segments
+            lines = cv2.HoughLinesP(edge, rho, theta, threshold, np.array([]),min_line_length, max_line_gap)
+            
+            try:
+
+                #print("first:",lines)
+                min_x=lines[0][0][1]
+                #print(min_x)
+                min_line=lines[0]
+                loose=0
+                for line in lines:
+                    [y1,x1,y2,x2]=line[0]
+                    if 260>x1>215 or 260>x2>215:
+                        angle = math.atan2(x2- x1, y2 - y1)*(180/np.pi)
+                        if 10>angle >-10:
+                            cv2.line(img,(y1,x1),(y2,x2),(255,0,0),5)  
+                            loose=1
+                            break
+                    
+                    if min(x1,x2)<min_x:
+                        min_x=min(x1,x2)
+                        min_line=line
+        
+                [y1,x1,y2,x2]=min_line[0]
+                angle = math.atan2(x2- x1, y2 - y1)*(180/np.pi)
+                cv2.line(img,(y1,x1),(y2,x2),(255,0,0),5)
+                if angle >10 or angle<-10 or (x1+x2)/2 <130 or loose==1:
+                    cv2.putText(img, "Loose", (520,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2,cv2.LINE_AA)
+                    
                 else:
-                    print("Good cap")
-                
-    except:
-        pass
+                    cv2.putText(img, "Good", (620,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2,cv2.LINE_AA)
+                return img
+            except:
+                pass
+    
+
+    #img[np.where(mask==0)] = 255
     return img
 
 def main():
@@ -116,7 +116,6 @@ def main():
     cam_1=setup_cam(0)
     global frame_1
 
-    
     while True:
         start_time = time.time()
 
@@ -128,7 +127,7 @@ def main():
         
         
         #frame_1= bottle_cap_inspection(cv2.resize(frame_1,(360,640))[0:160,0:360])
-        frame_1=bottle_cap_inspection(frame_1[0:290,0:720])
+        frame_1=bottle_cap_inspection(frame_1[0:300,0:720])
         FPS= 1.0 / (time.time() - start_time)
         cv2.putText(frame_1, str("%.2f" %FPS), (30,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2,cv2.LINE_AA)
         cv2.imshow("a",frame_1)
