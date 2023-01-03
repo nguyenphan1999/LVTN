ed9ff1c33882b1513f588774e84f4ce17081b255
nguyen <nguyen.phan241099@gmail.com>
2023-01-03 09:58:09 -0500

    final

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import cv2
 import time
 import numpy as np
 import math
-import subprocess
 from tkinter import *
 import tkinter as tk
 from multiprocessing import *
@@ -41,27 +40,6 @@ class Yolov4:
             return frame,1
         else:
             return frame,0
-
-def click_event(event, x, y, flags, params):
- 
-    # checking for left mouse clicks
-    if event == cv2.EVENT_LBUTTONDOWN:
- 
-        # displaying the coordinates
-        # on the Shell
-        print('Coorinate:',y, ' ', x)
-
- 
-    # checking for right mouse clicks    
-    if event==cv2.EVENT_RBUTTONDOWN:
- 
-        try: 
-            b = frame[y, x,0]
-            g = frame[y, x,1]
-            r = frame[y, x,2]
-            print(b,' ',g,' ',r)
-        except:
-            print(frame[y, x])
 
 def setup_cam_cap_and_fill(cam_id): 
     # Set up Webcam
@@ -127,7 +105,6 @@ def bottle_cap_inspection(img):
         lines = cv2.HoughLinesP(edge, rho, theta, threshold, np.array([]),min_line_length, max_line_gap)
         
         try:
-
             #print("first:",lines)
             min_x=lines[0][0][1]
             min_line=lines[0]
@@ -170,15 +147,15 @@ def bottle_fill_inspection(img):
 
 def detected(img,type):
     #type=0 <=> cap, type=1 <=> shape
-    
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     if type==1:
-        img=img[320:400,0:360]
-        fore = cv2.Canny(img,20,50)
+        img=img[320:400,60:300]
+        fore = cv2.Canny(img,10,30)
     elif type==0:
         img=cv2.resize(img,(360,640))
         img=img[560:640,0:360]
         fore = cv2.Canny(img,30,80)
-    img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE , (10,10))
     fore = cv2.morphologyEx(fore, cv2.MORPH_DILATE, kernel)   
     fore[0:5,0:360]=255
@@ -187,28 +164,25 @@ def detected(img,type):
     try:
         max_cnt=max(cnts, key=cv2.contourArea)
         cv2.drawContours(fore,[max_cnt],0,255,-1)
-        #cv2.imshow("h",fore)
+
         fore=fore[6:74]
         cnts,_= cv2.findContours(fore,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        max_cnt=max(cnts, key=cv2.contourArea)
         max_cnt=max(cnts, key=cv2.contourArea)
         area=cv2.contourArea(max_cnt)
         rect= cv2.boundingRect(max_cnt)
         
         x,y,w,h = rect
-        # fore=cv2.cvtColor(fore,cv2.COLOR_GRAY2BGR)
-        cv2.rectangle(fore,(x,y),(x+w,y+h),(0,255,0),2)
-        if (180>(x+w/2)>160) and area>8000 :
-            print(area)
+        if type==0:
+            if (180>(x+w/2)>160) and area>8000 :
+                return 0
 
-            #cv2.imshow("detected",img_original)
-            # _,imgh=cam_3.read()
-            # imgh=cv2.rotate(imgh, cv2.ROTATE_90_CLOCKWISE)
-            return 0
-            # cv2.imshow("detected2",imgh)
-        else:
-            return 1
-        # cv2.imshow("contour",fore)
+            else:
+                return 1
+        elif type==1:
+            if (120>(x+w/2)>100) and area>8000 :
+                return 0
+            else:
+                return 1
     except:
         return 1
 
@@ -216,10 +190,10 @@ def shape():
     
     cam_1 = setup_cam_shape(0)
     cam_2 = setup_cam_shape(1)
-    model = Yolov4(weights_dir='yolov4-custom_last.weights',
+    model = Yolov4(weights_dir='yolov4-custom_last_version6.weights',
                    cfg_dir='yolov4-custom.cfg',
                    names_dir='yolo.names')
-    print("aa")
+
     _, frame_1 = cam_1.read()
     _, frame_2 = cam_2.read()
     frame_1,_=model.detect(frame_1)
@@ -235,12 +209,12 @@ def shape():
         frame_2= cv2.rotate(frame_2, cv2.ROTATE_90_CLOCKWISE)
        
         detec=detected(frame_1,1)
+    
         if detec==0:
             
-            start_time=time.time()
             frame_1,classes_1=model.detect(frame_1)
             frame_2,classes_2=model.detect(frame_2)
-            print(classes_1, classes_2)
+            
             if classes_1==1 and classes_2==1:
                 result.value=int(str(order[k])+str(1))
             else:
@@ -248,13 +222,12 @@ def shape():
             if k==9:
                 k=0
             else: k=k+1
-            print("finised")
  
             b_1[:]=frame_1
             b_2[:]=frame_2
             frame_1=cam_1.read() #to flush out lastest frame
-            
-            print(time.time()-start_time)
+            sleep(1.5)
+
     cam_1.release()
     cam_2.release()
 def cap_and_fill():
@@ -265,12 +238,13 @@ def cap_and_fill():
 
     k=0
     while(start_stop[0]==1):
-        print("cc")
+        #print("cc")
         _, frame_3 = cam_3.read()
 
         frame_3= cv2.rotate(frame_3, cv2.ROTATE_90_COUNTERCLOCKWISE)
               
         detec=detected(frame_3,0)
+        
         if detec==0:
             bottle_cap=bottle_cap_inspection(frame_3)
             bottle_fill=bottle_fill_inspection(frame_3)
@@ -296,9 +270,10 @@ def cap_and_fill():
             frame_3=cv2.resize(frame_3,(360,640))  
             # cv2.imshow("cap_fill",frame_3)
             b_3[:]=frame_3
-            
             frame_3=cam_3.read()
+            sleep(1.9)
     cam_3.release()
+
 def run():
     
     if start_stop[0]==0:
@@ -310,8 +285,6 @@ def run():
         detect_cap_and_fill.daemon=True
         detect_shape.start()
         detect_cap_and_fill.start()
-        # detect_shape.join()
-        # detect_cap_and_fill.join()
     elif start_stop[0]==1:
         start_stop[0]=0
         btn.config(text="Start")
@@ -321,8 +294,7 @@ def update():
     if start_stop[0]==1:
         btn.config(text="Stop")
         btn["state"]="active"
-    #frame= cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-    #frame=cv2.resize(frame,(640,360))
+
     cv2image_1=cv2.cvtColor(b_1,cv2.COLOR_BGR2RGBA)
     imgtk_1=ImageTk.PhotoImage(image=Image.fromarray(cv2image_1))
     windows.one=imgtk_1
@@ -353,7 +325,7 @@ def update():
         elif str(result.value)[1]=="1":
             texts=texts+"Overfilled,"
         else:
-            texts=texts+"Good filled,"
+            texts=texts+"Good fill,"
 
         if str(result.value)[2]=="0":
             texts=texts+"Bad shape"
@@ -362,7 +334,6 @@ def update():
     
     text.insert(END, texts)
     windows.after(10,update)
-    
     
     
 if __name__ == "__main__":
@@ -376,8 +347,6 @@ if __name__ == "__main__":
     canvas_1.grid(column=1,row=0)
     canvas_2.grid(column=2,row=0)
     canvas_3.grid(column=0,row=0)
-
-    #canvas_1.pack()
     
     shared_1=Array(ctypes.c_uint8,640*360*3,lock=False)
     b_1=np.frombuffer(shared_1, dtype=np.uint8)
@@ -389,18 +358,11 @@ if __name__ == "__main__":
     b_3=np.frombuffer(shared_3, dtype=np.uint8)
     b_3=b_3.reshape((640,360,3))
     
-    
-
-
-    
     result=Value(ctypes.c_uint8,lock=False)
     result.value=9
     text= Text(windows,width=50,height=2)
     text.grid(column=1,row=1)
-    # text.insert(END, result.value)
-
-
-
+ 
     btn= Button(windows, text="Start",command=run)
     btn.grid(column=1,row=2)
     
